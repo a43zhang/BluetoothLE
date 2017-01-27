55c572c1292976b7a20ef80c12145364b28be336
leoybkim <leoybkim@gmail.com>
2017-01-27 17:21:10 -0500

    Enable Bluetooth

--- app/src/main/java/com/leoybkim/bluetoothle/ScannerActivity.java
+++ app/src/main/java/com/leoybkim/bluetoothle/ScannerActivity.java
@@ -1,13 +1,39 @@
 package com.leoybkim.bluetoothle;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ScannerActivity extends AppCompatActivity {
 
+    private static final String TAG = ScannerActivity.class.getSimpleName();
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter mBluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        // Initialize BluetoothAdapter through BluetoothManager
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Enable Bluetooth if disabled
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
 }
