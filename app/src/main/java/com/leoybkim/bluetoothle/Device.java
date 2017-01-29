c5edd363e95c3ab4da277d0b6e1293c3bf2b5396
leoybkim <leoybkim@gmail.com>
2017-01-29 06:49:03 -0500

    Connect to BLE devices

--- app/src/main/java/com/leoybkim/bluetoothle/Device.java
+++ app/src/main/java/com/leoybkim/bluetoothle/Device.java
@@ -1,5 +1,10 @@
 package com.leoybkim.bluetoothle;
 
+import android.bluetooth.BluetoothDevice;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by leo on 27/01/17.
  */
@@ -7,12 +12,18 @@ package com.leoybkim.bluetoothle;
 public class Device {
     private String deviceAddress;
     private String deviceName;
+    private BluetoothDevice device;
+    private List<UUID> uuids;
 
-    public Device(String address, String name) {
+    public Device(BluetoothDevice device, String address, String name, List<UUID> uuids) {
+        this.device = device;
         this.deviceAddress = address;
         this.deviceName = name;
+        this.uuids = uuids;
     }
 
     public String getDeviceAddress() { return deviceAddress; }
     public String getDeviceName() { return deviceName; }
+    public BluetoothDevice getDevice()  { return device; }
+    public List<UUID> getUUIDs() { return  uuids; }
 }
--- app/src/main/java/com/leoybkim/bluetoothle/ScannerActivity.java
+++ app/src/main/java/com/leoybkim/bluetoothle/ScannerActivity.java
@@ -1,28 +1,56 @@
 package com.leoybkim.bluetoothle;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
 
 public class ScannerActivity extends AppCompatActivity {
 
     private static final String TAG = ScannerActivity.class.getSimpleName();
     private static final int REQUEST_ENABLE_BT = 1;
     private static final long SCAN_PERIOD = 10000;
+    private static int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0;
+    private static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    // Base UUID for Adafruit BLE Bluetooth
+    // TODO: Create function to read device's UUID
+    public static UUID UART_UUID   = UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+    public static UUID TX_UUID     = UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+    public static UUID RX_UUID     = UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+    public static UUID CLIENT_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
     private BluetoothAdapter mBluetoothAdapter;
     private Handler mHandler;
-    private boolean mScanning;
+    private BluetoothGattCharacteristic tx;
+    private BluetoothGattCharacteristic rx;
+
     private ArrayList<Device> mDevices = new ArrayList<>();
     private DeviceAdapter mAdapter;
     private HashSet<String> mHashAddressSet = new HashSet<>();
@@ -32,15 +60,47 @@ public class ScannerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
 
+        // Requires Location permission to scan BLE
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
         // Initialize BluetoothAdapter through BluetoothManager
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
 
-        // Initialize Handler;
+        // Initialize Handler
         mHandler = new Handler();
+
+        // Initialize ListView
         ListView deviceListView = (ListView) findViewById(R.id.list);
         mAdapter = new DeviceAdapter(this, mDevices);
         deviceListView.setAdapter(mAdapter);
+
+        // Connect to device onClick
+        deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Device device = mAdapter.getItem(position);
+
+                if (device.getUUIDs().contains(UART_UUID)) {
+                    device.getDevice().connectGatt(getApplicationContext(), false, mGattCallback);
+                    Toast.makeText(getApplicationContext(), "Successfully connected " + device.getDeviceAddress().toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
     @Override
@@ -55,6 +115,13 @@ public class ScannerActivity extends AppCompatActivity {
             }
         }
 
+        // Enable Location services
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            Intent enableLocIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(enableLocIntent);
+        }
+
         scanDevice(true);
     }
 
@@ -64,16 +131,13 @@ public class ScannerActivity extends AppCompatActivity {
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    mScanning = false;
                     mBluetoothAdapter.stopLeScan(mLeScanCallback);
                     invalidateOptionsMenu();
                 }
             }, SCAN_PERIOD);
 
-            mScanning = true;
             mBluetoothAdapter.startLeScan(mLeScanCallback);
         } else {
-            mScanning = false;
             mBluetoothAdapter.stopLeScan(mLeScanCallback);
         }
         invalidateOptionsMenu();
@@ -84,14 +148,14 @@ public class ScannerActivity extends AppCompatActivity {
             new BluetoothAdapter.LeScanCallback() {
 
                 @Override
-                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+                public void onLeScan(final BluetoothDevice device, int rssi, final byte[] scanRecord) {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
 
                             if (!mHashAddressSet.contains(device.getAddress())) {
                                 mHashAddressSet.add(device.getAddress());
-                                mDevices.add(new Device(device.getAddress(), device.getName()));
+                                mDevices.add(new Device(device, device.getAddress(), device.getName(), parseUUIDs(scanRecord)));
                                 mAdapter.notifyDataSetChanged();
                                 Log.d(TAG, device.toString());
                             }
@@ -99,4 +163,120 @@ public class ScannerActivity extends AppCompatActivity {
                     });
                 }
             };
+
+
+    // Gatt callback
+    private BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        // Called whenever the device connection state changes, i.e. from disconnected to connected.
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                Toast.makeText(getApplicationContext(), "Connected!", Toast.LENGTH_LONG).show();
+                // Discover services.
+                if (!gatt.discoverServices()) {
+                    Toast.makeText(getApplicationContext(), "Failed to start discovering services!", Toast.LENGTH_LONG).show();
+                }
+            }
+            else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+                Toast.makeText(getApplicationContext(), "Disconnected!", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Connection state changed.  New state: ", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        // Called when services have been discovered on the remote device.
+        // It seems to be necessary to wait for this discovery to occur before
+        // manipulating any services or characteristics.
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(TAG, "Service discovery completed!");
+            }
+            else {
+                Log.d(TAG, "Service discovery failed with status: " + status);
+            }
+            // Save reference to each characteristic.
+            tx = gatt.getService(UART_UUID).getCharacteristic(TX_UUID);
+            rx = gatt.getService(UART_UUID).getCharacteristic(RX_UUID);
+            // Setup notifications on RX characteristic changes (i.e. data received).
+            // First call setCharacteristicNotification to enable notification.
+            if (!gatt.setCharacteristicNotification(rx, true)) {
+                Log.d(TAG, "Couldn't set notifications for RX characteristic!");
+            }
+            // Next update the RX characteristic's client descriptor to enable notifications.
+            if (rx.getDescriptor(CLIENT_UUID) != null) {
+                BluetoothGattDescriptor desc = rx.getDescriptor(CLIENT_UUID);
+                desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                if (!gatt.writeDescriptor(desc)) {
+                    Log.d(TAG, "Couldn't write RX client descriptor value!");
+                }
+            }
+            else {
+                Log.d(TAG, "Couldn't get RX client descriptor!");
+            }
+        }
+
+        // Called when a remote characteristic changes (like the RX characteristic).
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+            Log.d(TAG, "Received: " + characteristic.getStringValue(0));
+        }
+    };
+
+    // Filtering by custom UUID is broken in Android 4.3 and 4.4, see:
+    // http://stackoverflow.com/questions/18019161/startlescan-with-128-bit-uuids-doesnt-work-on-native-android-ble-implementation?noredirect=1#comment27879874_18019161
+    // This is a workaround function from the SO thread to manually parse advertisement data.
+    private List<UUID> parseUUIDs(final byte[] advertisedData) {
+        List<UUID> uuids = new ArrayList<UUID>();
+
+        int offset = 0;
+        while (offset < (advertisedData.length - 2)) {
+            int len = advertisedData[offset++];
+            if (len == 0)
+                break;
+
+            int type = advertisedData[offset++];
+            switch (type) {
+                case 0x02: // Partial list of 16-bit UUIDs
+                case 0x03: // Complete list of 16-bit UUIDs
+                    while (len > 1) {
+                        int uuid16 = advertisedData[offset++];
+                        uuid16 += (advertisedData[offset++] << 8);
+                        len -= 2;
+                        uuids.add(UUID.fromString(String.format("%08x-0000-1000-8000-00805f9b34fb", uuid16)));
+                    }
+                    break;
+                case 0x06:// Partial list of 128-bit UUIDs
+                case 0x07:// Complete list of 128-bit UUIDs
+                    // Loop through the advertised 128-bit UUID's.
+                    while (len >= 16) {
+                        try {
+                            // Wrap the advertised bits and order them.
+                            ByteBuffer buffer = ByteBuffer.wrap(advertisedData, offset++, 16).order(ByteOrder.LITTLE_ENDIAN);
+                            long mostSignificantBit = buffer.getLong();
+                            long leastSignificantBit = buffer.getLong();
+                            uuids.add(new UUID(leastSignificantBit,
+                                    mostSignificantBit));
+                        } catch (IndexOutOfBoundsException e) {
+                            // Defensive programming.
+                            // Log.e(LOG_TAG, e.toString());
+                            continue;
+                        } finally {
+                            // Move the offset to read the next uuid.
+                            offset += 15;
+                            len -= 16;
+                        }
+                    }
+                    break;
+                default:
+                    offset += (len - 1);
+                    break;
+            }
+        }
+        return uuids;
+    }
 }
